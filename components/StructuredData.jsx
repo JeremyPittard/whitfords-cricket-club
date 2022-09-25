62f70d68beb5ee43249fe50ebf3715f6f20dd2fe
Jeremy Pittard <jeremy@jpittard.net>
2022-09-25 18:01:06 +0800

    added structured data

---
+++ components/StructuredData.jsx
@@ -0,0 +1,16 @@
+{
+  /* <script type="application/ld+json"> */
+}
+const structuredData = () => {
+  const data = {
+    "@context": "https://schema.org",
+    "@type": "SportsTeam",
+    name: "",
+    url: "",
+    logo: "",
+    sameAs: ["", ""],
+  };
+
+  return data;
+};
+// </script>
